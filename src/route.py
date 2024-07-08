1f72d8f25929ddcefd3f8fb22cc6801eea2e30a0
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-07-08 20:05:47 +0200

    Added POST and GET method to get and save variables into DB.

--- src/route.py
+++ src/route.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.database.models import Prediction
+from src.database.db import get_db
 
 route_bp = Blueprint('route', __name__)
 
@@ -9,3 +11,21 @@ def test():
         {'message': 'Hello World!!!'}
     ), 200
 
+
+@route_bp.route('/predictions', methods=['POST', 'GET'])
+def predictions():
+    db = next(get_db())
+    if request.method == 'GET':
+        predictions = db.query(Prediction).all()
+        return jsonify([prediction.to_dict() for prediction in predictions]), 200
+
+    if request.method == 'POST':
+        data = request.get_json()
+
+        predictions = Prediction(**data)
+
+        db.add(predictions)
+        db.commit()
+        db.refresh(predictions)
+
+        return jsonify(predictions.to_dict()), 201
